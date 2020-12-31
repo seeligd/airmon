298a28c80dde7ef92378857db78080e7061a03da
David Seelig <dseelig@teslamotors.com>
2020-12-30 21:05:17 -0800

    only return last 24 hours

--- web.py
+++ web.py
@@ -3,6 +3,9 @@ from flask import Response
 from flask import send_from_directory, stream_with_context
 import csv
 from io import StringIO
+import datetime
+from datetime import timedelta
+import dateutil.parser
 
 app = Flask(__name__)
 
@@ -23,6 +26,9 @@ def send_csv():
         with open("./all.csv", "r") as f:
             data = StringIO() 
 
+            # only return values from within the last 24 hours
+            earliest = datetime.datetime.now() - timedelta(hours=-24)
+
             r = csv.reader(f, delimiter="\t")
             for i, row in enumerate(r):
                 if i == 0:
@@ -31,7 +37,8 @@ def send_csv():
                     data.seek(0)
                     data.truncate(0)
                 else:
-                    if is_valid_data(row[0]):
+                    dateVal = row[0].split(",")[0]
+                    if dateutil.parser.isoparse(dateVal) > earliest:
                         data.write("".join(row) + "\n")
                         yield data.getvalue()
                         data.seek(0)
