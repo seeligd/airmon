fc7ab05f3f59836130f28aba58ff30163b73a43d
David Seelig <dseelig@teslamotors.com>
2020-12-30 21:49:30 -0700

    WIP

--- web.py
+++ web.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from flask import send_from_directory
+from flask import Response
+from flask import send_from_directory, stream_with_context
+import csv
+from io import StringIO
 
 app = Flask(__name__)
+
+def is_valid_data(row):
+    return True
 
 @app.route('/graph')
 def send_graph():
@@ -13,7 +19,24 @@ def send_data():
 
 @app.route('/all.csv')
 def send_csv():
-    return send_from_directory('.', "all.csv")
+    def generate():
+        with open("./all.csv", "r") as f:
+            data = StringIO() 
+
+            r = csv.reader(f, delimiter="\t")
+            for i, row in enumerate(r):
+                if i == 0:
+                    data.write("".join(row) + "\n")
+                    yield data.getvalue()
+                    data.seek(0)
+                    data.truncate(0)
+                else:
+                    if is_valid_data(row[0]):
+                        data.write("".join(row) + "\n")
+                        yield data.getvalue()
+                        data.seek(0)
+                        data.truncate(0)
+    return Response(generate(), mimetype='text/csv')
 
 @app.route('/')
 @app.route('/chart')
