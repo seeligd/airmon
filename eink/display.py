9ae5591065f908e9b4944570c2b0256fcd849567
David Seelig <dseelig@teslamotors.com>
2021-01-03 19:14:41 -0700

    update display text

--- eink/display.py
+++ eink/display.py
@@ -5,13 +5,17 @@ import datetime
 import logging
 import requests
 import urllib.request, json
+import urllib
 import RPi.GPIO as GPIO
 
 TEST = False
 
 GPIO.setmode(GPIO.BCM)
 
-logging.basicConfig(level=logging.DEBUG)
+OUTDOOR=('http://rhubarb:5000/static/eink_output.png', 'http://rhubarb:5000/summary')
+INDOOR=('http://burdock:5000/static/eink_output.png', 'http://burdock:5000/summary')
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s')
 
 if not TEST:
     epd = epd2in7.EPD()
@@ -33,10 +37,14 @@ GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def updateDisplay(string):
+def updateDisplay(url):
+    summary = getSummary(url)
     draw = ImageDraw.Draw(image)
-    draw.text((20, 50), string, font = font, fill = 0)
-    #draw.rectangle((epd2in7.EPD_WIDTH/2-10, epd2in7.EPD_HEIGHT/2-10, epd2in7.EPD_WIDTH/2+10, epd2in7.EPD_HEIGHT/2+10), fill = 0)
+    draw.text((5, 50), summary, font = fontSmall, fill = 0)
+    addName(url, draw)
+
+    image.save("output.png", "PNG")
+
     if not TEST:
         epd.Clear(0xFF)
         epd.display(epd.getbuffer(image))
@@ -45,24 +53,45 @@ def download(fname, url):
     r = requests.get(url, allow_redirects=True)
     open(fname, 'wb').write(r.content)
 
-def getSummary():
-    with urllib.request.urlopen("http://rhubarb:5000/summary") as url:
-        #data = json.loads(url.read().decode())
-        return url.read().decode()
-        #print(data)
+def getSummary(url):
+    try:
+        with urllib.request.urlopen(url) as url:
+            #data = json.loads(url.read().decode())
+            return url.read().decode()
+            #print(data)
+    except urllib.error.HTTPError:
+        return "{}"
 
-def getGraph():
+def addName(url, draw):
+    urlMap = {
+            INDOOR[0]: 1,
+            OUTDOOR[0]: 2,
+            INDOOR[1]: 3,
+            OUTDOOR[1]: 4,
+    }
+    urlMapName = {
+            INDOOR[0]: 'inside',
+            OUTDOOR[0]: 'outside',
+            INDOOR[1]: 'inside stats',
+            OUTDOOR[1]: 'outside stats',
+    }
+
+    draw.text((2, 162), str(urlMap.get(url)) + "/4 - " + urlMapName.get(url), font = fontMicro, fill = 0)
+
+def getGraph(url):
     #urllib.error.HTTPError: HTTP Error 500: INTERNAL SERVER ERROR
-    logging.info("downloading graph")
+    logging.info("downloading graph: " + url)
     fname = 'outside.png'
-    url = 'http://rhubarb:5000/static/eink_output.png'
     download(fname, url)
-
     image = Image.open(fname)
 
     draw = ImageDraw.Draw(image)
 
-    response = json.loads(getSummary())
+    summaryURL = OUTDOOR[1]
+    if url == INDOOR[0]:
+        summaryURL = INDOOR[1]
+
+    response = json.loads(getSummary(summaryURL))
     lastUpdated = response.get('Updated')
     if lastUpdated:
         lastUpdated = datetime.datetime.fromisoformat(lastUpdated).strftime("%a %b %d %I:%M%p")
@@ -75,6 +104,7 @@ def getGraph():
         draw.text((113, 0), str(aqiNow), font = font, fill = 0)
 
     draw.text((160, 4), lastUpdated, font = fontMicro, fill = 0)
+    addName(url, draw)
     image.save("output.png", "PNG")
 
     if not TEST:
@@ -83,7 +113,7 @@ def getGraph():
 
 def main():
     lastRun = time.time() - 100
-    lastOne = None
+    lastOne = OUTDOOR[0]
 
     while True:
         key1state = GPIO.input(key1)
@@ -93,29 +123,37 @@ def main():
 
         if key1state == False:
             print('Key1 Pressed')
+            lastOne = INDOOR[0]
+            getGraph(lastOne)
             time.sleep(0.2)
         elif key2state == False:
             print('Key2 Pressed')
+            lastOne = OUTDOOR[0]
+            getGraph(lastOne)
             time.sleep(0.2)
         elif key3state == False:
             print('Key3 Pressed')
-            lastOne = "Graph"
-            getGraph()
+            lastOne = INDOOR[1]
+            updateDisplay(lastOne)
             time.sleep(0.2)
         elif key4state == False:
             print('Key4 Pressed')
-            lastOne = "Stats"
-            updateDisplay(getSummary())
+            lastOne = OUTDOOR[1]
+            updateDisplay(lastOne)
             time.sleep(0.2)
 
         else:
             if time.time() - lastRun > UPDATE_INTERVAL:
                 lastRun = time.time()
                 time.sleep(.2)
-                if lastOne == "Graph":
-                    getGraph()
-                elif lastOne == "Stats":
-                    updateDisplay(getSummary())
+                if lastOne in [INDOOR[0], OUTDOOR[0]]:
+                    getGraph(lastOne)
+                elif lastOne in [INDOOR[1], OUTDOOR[1]]:
+                    updateDisplay(lastOne)
 
 if __name__ == '__main__':
-    main()
+    if TEST:
+        updateDisplay(OUTDOOR[1])
+        #getGraph(OUTDOOR[0])
+    else:
+        main()
